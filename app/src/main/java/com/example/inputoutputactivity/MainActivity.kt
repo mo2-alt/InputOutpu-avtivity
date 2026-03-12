f8019e46bcc05c989e16ca4e01303c74a905b9d3
mo2-alt <mohamednur2006@gmail.com>
2026-03-12 09:12:41 +0200

    added the button clicked message

---
+++ app/src/main/java/com/example/inputoutputactivity/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.inputoutputactivity
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        // get the button using the id we set on the user interface
+        val Clickmebutton = findViewById<Button>(R.id.Clickmebutton)
+        // add code to the button that happens whn its clicked
+        Clickmebutton?.setOnClickListener {Toast.makeText(this@MainActivity,"Button Clicked", Toast.LENGTH_LONG).show
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+
+}
