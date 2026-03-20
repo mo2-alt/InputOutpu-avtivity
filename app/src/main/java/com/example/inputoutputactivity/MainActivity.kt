789aeae9145af6dab0d731820ed3a9c8492e38ba
mo2-alt <mohamednur2006@gmail.com>
2026-03-20 10:27:01 +0200

    corrected all the errors

--- app/src/main/java/com/example/inputoutputactivity/MainActivity.kt
+++ app/src/main/java/com/example/inputoutputactivity/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.inputoutputactivity
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,41 +12,53 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    enum class Language {
+        ENGLISH,
+        NDEBELE,
+        PEDI,
+        SOTHO,
+        SWATI,
+        TSONGA,
+        TSWANA,
+        VENDA,
+        XHOSA,
+        ZULU,
+        AFRIKAANS
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         // get the button using the id we set on the user interface
         val Clickmebutton = findViewById<Button>(R.id.Clickmebutton)
-        val welcomeText = findViewById<TextView>(R.id.clickbutton)
-        val nameTextField = findViewById<EditText>(R.id.NameText)
+        val welcomeText = findViewById<TextView>(R.id.displayTxt)
+        val nameTextField = findViewById<EditText>(R.id.editTextText)
+        val zuluSwitch = findViewById<Switch>(R.id.switch1)
 
         // add code to the button that happens when it id clicked
-        clickbutton?.setOnClickListener
+        Clickmebutton?.setOnClickListener() {
 
-        var greeting: string
-        var zulu: Boolean = zuluSwitch.isChecked
-        if (zulu)
-            greeting = "Mogammad, ${nameTextfield.text}!"
-        if ((nameTextField.text.tostring() == "Mogammad"   ||
-                    nameTextField.text.toString() == "Mogammad")
-            && age > 20)
-            greeting = "Yo, ${nameTextField.text}!"
+            var greeting: String
+            var zulu: Boolean = zuluSwitch.isChecked
+            var age: Int = 20
 
-        Toast.makeText(
-            context = this@MainActivity,
-            text = "Button Clicked", duration = Toast.LENGTH_LONG
-        ).show()
-        welcomeText.text = *welcome * + NameTextField.text
+            Toast.makeText(this@MainActivity, "Button Clicked", Toast.LENGTH_LONG).show()
 
+            if (zulu)
+                greeting = "Mogammad, ${nameTextField.text}!"
+            if ((nameTextField.text.toString() == "Mogammad" ||
+                        nameTextField.text.toString() == "Mogammad")
+                && age > 20
+            )
+                greeting = "Yo, ${nameTextField.text}!"
 
-        Clickmebutton?.setOnClickListener {Toast.makeText(this@MainActivity,"Button Clicked", Toast.LENGTH_LONG).show
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            welcomeText.text = " welcome " + nameTextField.text
+        }
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
         }
     }
-
-
-}
